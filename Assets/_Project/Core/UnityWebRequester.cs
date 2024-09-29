e1d6edb86414ae14a3aa82d1ea47b10ceeddcc8d
gggitttLocal <rsepmee@gmail.com>
2024-09-29 23:50:15 +0300

    feat: set project settings, run test script

---
+++ Assets/_Project/Core/UnityWebRequester.cs
@@ -0,0 +1,15 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Core
+{
+public class UnityWebRequester : MonoBehaviour
+{
+    [Button]
+    public void Test( )
+    {
+        Debug.Log( $"<color=cyan> {nameof( Test )} </color>" );
+
+    }
+}
+}
